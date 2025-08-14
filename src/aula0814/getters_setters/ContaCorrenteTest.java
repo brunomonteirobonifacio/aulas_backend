125c975bfc9ddb76a2ad568ad67b85a1098f58eb
bruno.343923 <bruno.343923@alunosatc.edu.br>
2025-08-14 20:42:12 -0300

    Aula de 14/08/2025: Getters e Setters

--- src/aula0814/getters_setters/ContaCorrenteTest.java
+++ src/aula0814/getters_setters/ContaCorrenteTest.java
@@ -2,9 +2,15 @@ package aula0814.getters_setters;
 
 public class ContaCorrenteTest {
     public static void main(String[] args) {
-        ContaCorrente conta = new ContaCorrente();
+        ContaCorrente conta1 = new ContaCorrente();
+        conta1.setTitular("Bruno");
+        conta1.depositar(200);
+        conta1.mostrarInfo();
 
-        conta.setTitular("Bruno");
-        conta.mostrarInfo();
+        System.out.println();
+
+        ContaCorrente conta2 = new ContaCorrente("Bruno 2", 1);
+        conta1.depositar(100);
+        conta2.mostrarInfo();
     }
 }
