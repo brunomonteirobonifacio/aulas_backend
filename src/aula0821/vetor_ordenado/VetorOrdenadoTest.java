3b160322a6480a775e1d829db1c6b5da93f338a7
Bruno Monteiro Bonifácio <brunombonifacio@gmail.com>
2025-08-24 22:33:42 -0300

    Criado teste para o método excluir de VetorOrdenado

--- src/aula0821/vetor_ordenado/VetorOrdenadoTest.java
+++ src/aula0821/vetor_ordenado/VetorOrdenadoTest.java
@@ -10,6 +10,7 @@ public class VetorOrdenadoTest {
         System.out.println();
         testPesquisaBinaria(vetorOrdenado);
         System.out.println();
+        testExcluir(vetorOrdenado);
     }
 
     public static void testInserir(VetorOrdenado<Integer> vetorOrdenado) {
@@ -42,5 +43,15 @@ public class VetorOrdenadoTest {
         System.out.println("Teste pesquisa binária para o valor 2: " + vetorOrdenado.pesquisaBinaria(2));
         System.out.println("Teste pesquisa binária para o valor 7: " + vetorOrdenado.pesquisaBinaria(7));
         System.out.println("Teste pesquisa binária para o valor 0: " + vetorOrdenado.pesquisaBinaria(0));
+    }
+
+    public static void testExcluir(VetorOrdenado<Integer> vetorOrdenado) {
+        boolean exclusaoBemSucedida = vetorOrdenado.excluir(2);
+        System.out.println("Exclusão deve ser bem sucedida: " + exclusaoBemSucedida);
+        vetorOrdenado.imprimir();
+
+        exclusaoBemSucedida = vetorOrdenado.excluir(2);
+        System.out.println("Exclusão deve ser mal sucedida (já não existe mais o valor no vetor): " + exclusaoBemSucedida);
+        vetorOrdenado.imprimir();
     }
 }
