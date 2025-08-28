ad129bb93dde52d1f052547bff8b5d37c8677268
bruno.343923 <bruno.343923@alunosatc.edu.br>
2025-08-28 20:14:21 -0300

    Aula de 28/08/2025: Composição - Pratica 02 com Motor e CarroZ - alterado tipo do motor

--- src/aula0828/composicao/pratica02/CarroTest.java
+++ src/aula0828/composicao/pratica02/CarroTest.java
@@ -2,7 +2,7 @@ package aula0828.composicao.pratica02;
 
 public class CarroTest {
     public static void main(String[] args) {
-        Carro carro = new Carro("Kwid", new Motor(79, "4 cilindros"));
+        Carro carro = new Carro("Kwid", new Motor(79, "0.6"));
 
         carro.ligarCarro();
         carro.desligarCarro();
