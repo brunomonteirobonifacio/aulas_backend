7c8a9099eb452e498fb0ff499cafd529e1c6159d
bruno.343923 <bruno.343923@alunosatc.edu.br>
2025-09-04 20:18:36 -0300

    Aula de 04/09/20258 - Prática 02: interfaces

---
+++ src/aula0904/interfaces/pratica02/Boleto.java
@@ -0,0 +1,13 @@
+package aula0904.interfaces.pratica02;
+
+public class Boleto implements Pagamento {
+    @Override
+    public String getDescricao() {
+        return "Boleto";
+    }
+
+    @Override
+    public void pagar(double valor) {
+        System.out.printf("R$%.2f pagos com boleto\n", valor);
+    }
+}
---
+++ src/aula0904/interfaces/pratica02/Pagamento.java
@@ -0,0 +1,6 @@
+package aula0904.interfaces.pratica02;
+
+public interface Pagamento {
+    void pagar(double valor);
+    String getDescricao();
+}
